84211a420c3d59964d821e52e6c304332f416489
Your Name <you@example.com>
2023-10-30 08:59:26 -0500

    created the crud methods, fixed mistakes in app routing and ts, created the form in create product

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { CoffeeService } from 'src/app/services/coffee.service';
 
 @Component({
   selector: 'app-create-product',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent {
+
+    newProduct: Product = new Product();
+
+    constructor(private productService: CoffeeService, private router: Router){}
+
+    ngOnInit(): void{}
+
+    onSubmit(){
+      this.productService.createNewProduct(this.newProduct).subscribe(response => {
+        console.log(response);
+        this.router.navigateByUrl("/products")
+      })
+    }
 
 }

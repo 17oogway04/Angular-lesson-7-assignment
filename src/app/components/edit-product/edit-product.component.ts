90e43acf28a0ba189f203121ff8c08ba47888c34
Your Name <you@example.com>
2023-10-31 08:51:02 -0500

    completed all cruds method, ready for styling

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { CoffeeService } from 'src/app/services/coffee.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent {
+
+  id: number = 0;
+
+  currentProduct: Product = new Product()
+
+  constructor(private productService: CoffeeService, private actRoute: ActivatedRoute, private router: Router){}
+
+  ngOnInit(): void{
+    const routeId = this.actRoute.snapshot.paramMap.get("id") ?? "";
+    this.id = parseInt(routeId);
+    this.productService.getProductbyID(this.id).subscribe(foundProduct => {
+      this.currentProduct = foundProduct;
+    })
+  }
+
+  onSubmit(){
+    this.productService.editProductByID(this.id, this.currentProduct). subscribe(edittedProduct => {
+      this.router.navigateByUrl("/products");
+    })
+  }
 
 }
--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { CoffeeService } from 'src/app/services/coffee.service';
 
@@ -11,11 +11,25 @@ import { CoffeeService } from 'src/app/services/coffee.service';
 export class ProductDetailsComponent implements OnInit {
   id: number = 0;
 
+
   currentProduct: Product = new Product()
 
-  constructor(private productService: CoffeeService, private actRoute: ActivatedRoute) { }
+  constructor(private productService: CoffeeService, private actRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-
+    const routeId = this.actRoute.snapshot.paramMap.get("id") ?? "";
+    this.id = parseInt(routeId);
+    this.productService.getProductbyID(this.id).subscribe(foundProduct => {
+      console.log(foundProduct);
+      this.currentProduct = foundProduct;
+    })
   }
+
+  onDelete(id: number){
+    this.productService.deleteProductByID(id).subscribe(response => {
+      this.router.navigateByUrl("/products");
+    })
+  }
+
+  
 }
